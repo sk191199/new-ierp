2e97ecb411abe09a1d328b0e4f0e241171014846
Sasijoy <124587665+sk191199@users.noreply.github.com>
2026-08-22 13:28:39 +0530

    feat: update lead status options to include 'Contacted'

--- src/constants/statuses.ts
+++ src/constants/statuses.ts
@@ -10,6 +10,7 @@ export const DOCUMENT_STATUSES = {
 
 export const LEAD_STATUSES = {
   new: "New",
+  contacted: "Contacted",
   qualified: "Qualified",
   disqualified: "Disqualified",
 } as const;
